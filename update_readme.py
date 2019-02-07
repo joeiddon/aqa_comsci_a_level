d9621e7dde705417a2d055116f9dc2492a896335
joeiddon <joedominiciddon@gmail.com>
2019-02-07 09:37:06 +0000

    m

--- update_readme.py
+++ update_readme.py
@@ -1,4 +1,4 @@
-import os
+import os,urllib.parse
 with open('README.md', 'w') as f:
     for s in os.listdir('notes'):
-        f.write(f'[{s}](notes/\'{s}\')\n')
+        f.write(f'[{s}](notes/{urllib.parse.quote(s)})\n')
